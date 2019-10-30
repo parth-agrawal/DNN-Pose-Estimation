16ef7857eed6c72b9d8440ef9679f4c0cd60f609
Parth Agrawal <parth.agrawal@outlook.com>
2019-10-29 21:24:59 -0400

    Moved event 4 images and CollectedData csv into labeled-data. Ran check_labels and generated labeled images. Doesn't seem to be a way to merge the data of all 5 labeled frame events, so for now will run on event 4 and hope to add to training set later.

--- DeepLabCut/reinstall.sh
+++
@@ -1,3 +0,0 @@
-pip uninstall deeplabcut
-python3 setup.py sdist bdist_wheel
-pip install dist/deeplabcut-2.1.1-py3-none-any.whl
--- create_project.py
+++
@@ -1,7 +0,0 @@
-import deeplabcut
-
-task = 'Reaching'
-experimenter = 'Parth'
-
-video = ["c:/users/parth/desktop/dnn-pose-estimation/Videos/rat1.mp4"]
-path_config_file=deeplabcut.create_new_project(task,experimenter,video, working_directory='C:/Users/parth/Desktop/DNN-Pose-Estimation',copy_videos=False)
